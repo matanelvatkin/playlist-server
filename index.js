35cdfdb2b69936d695e468e3518f4967a76a56af
matanelVatkin <matanelVatkin@gmail.com>
2023-02-21 15:12:58 +0200

    web done 1.1.3

--- index.js
+++ index.js
@@ -8,14 +8,14 @@ const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// const corsOptions ={
-//     origin:'http://localhost:3000', 
-//     credentials:true,
-//     optionSuccessStatus:200
-// }
+const corsOptions ={
+    origin:'https://playlist-client.onrender.com/', 
+    credentials:true,
+    optionSuccessStatus:200
+}
 
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptions));
 
 const swaggerOptions = {
   swaggerDefinition: {

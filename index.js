75d4cc8e20b0c8e8eea0fcf3f13b792f55a60e34
matanelVatkin <matanelVatkin@gmail.com>
2023-02-21 15:17:53 +0200

    web done 1.1.4

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 const corsOptions ={
-    origin:'https://playlist-client.onrender.com/', 
+    origin:'https://playlist-client.onrender.com', 
     credentials:true,
     optionSuccessStatus:200
 }

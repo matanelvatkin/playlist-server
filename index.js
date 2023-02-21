48f76e5e50b0e874f244c62194477f00afcd1437
matanelVatkin <matanelVatkin@gmail.com>
2023-02-21 18:10:55 +0200

    web done 1.2.0

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ const swaggerUi = require("swagger-ui-express");
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "https://playlist-client.onrender.com"); 
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
+  next();
+});
 app.use(cors());
 
 

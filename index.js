a82584dcaaad5ae67660c90d12c3c2cc69769878
matanelVatkin <matanelVatkin@gmail.com>
2023-02-21 13:27:26 +0200

    web done 1.1.1

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 
 const corsOptions ={
     origin:'http://localhost:3000', 
-    credentials:true,            //access-control-allow-credentials:true
+    credentials:true,
     optionSuccessStatus:200
 }
 

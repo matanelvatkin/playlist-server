85f1c4e5b4c1251d238ba9091df771d539d622a1
matanelVatkin <matanelVatkin@gmail.com>
2023-02-21 13:43:19 +0200

    web done 1.1.2

--- index.js
+++ index.js
@@ -8,14 +8,14 @@ const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const corsOptions ={
-    origin:'http://localhost:3000', 
-    credentials:true,
-    optionSuccessStatus:200
-}
+// const corsOptions ={
+//     origin:'http://localhost:3000', 
+//     credentials:true,
+//     optionSuccessStatus:200
+// }
 
 app.use(express.json());
-app.use(cors(corsOptions));
+app.use(cors());
 
 const swaggerOptions = {
   swaggerDefinition: {
